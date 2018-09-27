da0aa6890658a0806e4b92638e220aef654cab21
Guilherme Franca <guilherme.cipriano@ccc.ufcg.edu.br>
2018-09-27 19:32:24 -0300

    elementos base hash sha265

---
+++ C/sha256.h
@@ -0,0 +1,24 @@
+#ifndef SHA256_H
+#define SHA256_H
+
+/*************************** HEADER FILES ***************************/
+#include <stddef.h>
+
+/****************************** MACROS ******************************/
+#define SHA256_BLOCK_SIZE 64        
+
+/**************************** DATA TYPES ****************************/
+typedef unsigned char BYTE;            
+typedef unsigned int  WORD;            
+
+typedef struct {
+	BYTE data[64];
+	WORD datalen;
+	unsigned long long bitlen;
+	WORD state[8];
+} SHA256_CTX;
+
+/*********************** FUNCTION DECLARATIONS **********************/
+BYTE[64] sha256(char* text);
+
+#endif  
