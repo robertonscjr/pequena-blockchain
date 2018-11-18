c23e1dd3b3d667f77fb20a3840e3882bbd74fd5b
Victor Miranda <victruu@outlook.com>
2018-11-18 16:31:37 -0300

    Melhorando legibilidade do codigo

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -1,5 +1,7 @@
 :- initialization(main).
 
+%! ----------- Funcoes relacionadas a Bloco -----------
+
 %! Cria Bloco Genesis
 buildBlocoGenesis(0, 0, [(7,0,1000), (7,1,1000)] , "before", "hash").
 
@@ -8,6 +10,8 @@ buildBloco(Index, TimeStamp, Transacoes ,HashAnterior, Hash).
 
 %! Cria Bloco e verifica se o Index já existe na blockchain, se existir verifica na proxima posicao até chegar em um Index nao existente na blockchain, quando encontra constroi o bloco.
 buildBlocoNaBlockchain(N, TimeStamp, Transacoes, HashAnterior, Hash):- buildBloco(N, _,_,_,_) -> I is N+1, buildBlocoNaBlockchain(I) ; buildBloco(N, TimeStamp, Transacoes, HashAnterior, Hash).
+
+%! ----------- Funcoes relacionadas a Transacao -----------
 
 %! Cria transacao pendente e a coloca em formato de Lista.
 buildTransacaoPendente(Sender, Receiver, Valor, [Sender,Receiver,Valor]).
@@ -18,21 +22,22 @@ buildListaTransacaoPendentes(Transacao):- Transacao = [].
 %! Adiciona uma nova transacao na lista de transações pendentes.
 addTransacaoPendente(TransacaoPendente, TransacoesPen, [TransacoesPen|TransacaoPendente]).
 
-%! Cria uma blockchain
-buildBlockchain(Cadeia) :- Cadeia = [].
+%! Obter transacao
+obterTransacao()
 
 %! Transfere as transacoes pendentes para a lista de transcoes feitas no bloco.
 transfereTransacoesPendentes([],L1,L2).
 transfereTransacoesPendentes([Head|Tail],L1,[Head|L2]) :-
    transfereTransacoesPendentes(Tail,L1,L2).
 
+%! ----------- Funcoes relacionadas a Blockchain -----------
 
-
-buildGenesis() :-
-   buildBloco([buildTransacao(7,0,1000)]).
+%! Cria uma blockchain
+buildBlockchain(Cadeia) :- Cadeia = [].
 
 %! Adiciona bloco na blockchain
 addBloco(Cadeia, Bloco, [Bloco|Cadeia]).
+
 
 main :-
 buildBlockchain(Cadeia), buildBlocoGenesis(I, T, Transacoes, HashA, Hash), Genesis = [I, T, Transacoes, HashA, Hash], addBloco(Cadeia, Genesis, Blockchain).
